2dd2278550c00824f47b6b12c73892b6cdf02475
Emerald <emerald.xiaoyuliang@gmail.com>
2020-01-14 03:50:37 -0500

    edit the web elements and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dashboard-angular-firebase';
+
+  ngOnInit() {
+    particlesJS.load('particles', 'assets/data/particles.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
