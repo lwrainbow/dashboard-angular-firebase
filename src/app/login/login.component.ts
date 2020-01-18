d627afbd255479afb04057851f8733eba1c974a8
Emerald <emerald.xiaoyuliang@gmail.com>
2020-01-18 00:02:58 -0500

    implement login function and show the error message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  // Booleen var if the page still loading
   loading = false;
+  // action hold the types of string login or string signup, default is string login
+  action: 'login' | 'signup' = 'login';
+  // error meaage
+  error: string;
 
   constructor(private afAuth: AngularFireAuth, private router: Router) { }
 
@@ -17,12 +22,19 @@ export class LoginComponent implements OnInit {
 
   async onSubmit(form: NgForm) {
     this.loading = true;
+    this.error = null;
 
     const { firstName, lastName, email, password } = form.value;
 
+    let resp;
     try {
-      const resp = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
-      await resp.user.updateProfile({ displayName: `${firstName} ${lastName}`});
+      // if it is SignUp page then create user, if is Login page then login
+      if (this.isSignUp) {
+        resp = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+        await resp.user.updateProfile({ displayName: `${firstName} ${lastName}`});
+      } else {
+        resp = await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+      }
 
       // get id from response
       const uid = resp.user.uid;
@@ -34,7 +46,16 @@ export class LoginComponent implements OnInit {
       form.reset();
     } catch(error) {
       console.log(error.message);
+      this.error = error.message;
     }
     this.loading = false;
+  }
+
+  get isLogin() {
+    return this.action === 'login';
+  }
+
+  get isSignUp() {
+    return this.action === 'signup';
   }
 }
